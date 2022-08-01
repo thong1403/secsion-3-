a95906ccdf58d2e472610d08fbc0e1421d919396
thong1403 <110025148+thong1403@users.noreply.github.com>
2022-08-01 22:59:55 +0700

    bài tập trên lớp

---
+++ section 3 bt/add.js
@@ -0,0 +1,94 @@
+//ex 1
+// let a =Number(prompt("nhập vào một số nguyên"));
+// let b = 0;
+// if(a > 0 && Number.isInteger(a)){
+//     for (let i = 0; i<= a ;i = i + 1){
+//         b= b + i ;
+//     }alert(`B = ${b}`);
+// }else alert("nhap lai")
+
+//ex2
+// let a = Number (prompt("nhập vào một số"));
+// let b = 1;
+// if( a > 0 && Number.isInteger (a)){
+//     for(let i = 1; i <= a ; i = i + 1){
+//         b = b * i ;
+//     }alert(`B = ${b}`);
+
+// }else alert("nhập lại");
+
+//ex3
+// let a = Number(prompt("nhập vào một số n"));
+// let b = 0;
+// if( a> 0 && Number.isInteger(a)){
+//     for (let i = 1 ; i <= a ; i = i +1){
+//         b = b + 1/ i;
+//     }alert(`B = ${b}`);
+// }else alert("nhập lại");
+
+//ex4
+// let a = Number(prompt("nhập một số"));
+// let b = 0;
+// if (a >= 2 && Number.isInteger(a)){
+//     for (let i = 2 ; i<= a; i = i + 1 ){
+//         b = b +1 / ( i-1)* 1/i;
+//     }alert (` B = ${b}`);
+// }else alert("nhập lại");
+
+//ex 5
+// let a = Number(prompt("nhập số bất kì"));
+// if (a> 0 && Number.isInteger(a)){
+//     for (let i = 1 ; i < 11 ; i = i + 1){
+        
+//         console.log(`${a} );
+//     }
+
+//      }else console.log("nhập lại ");
+
+//      }else console.log("nhập lại");
+// ex 6
+// let n1 = 0, n2 = 1, nextTerm;
+// console.log('Fibonacci Series:');
+// for (let i = 1 ; i<= 13 ; i++){
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+//ex 7 
+// let a = Number(prompt("cho người dùng nhập vào một số "));
+// let b = 0;
+
+// if( a > 0 && Number.isInteger(a)){
+//     for (var i = 0
+//          ; i <= a ; i = i+ 1){
+//         if(i % 2 === 0){
+//         console.log(`${i}- chẵn`);
+//     }else {
+//         console.log(`${i}-lẻ`);
+//     }
+//     }}
+
+//ex 8
+// let a = Number(prompt("cho người dungf nhập "));
+
+// if(a>0 && Number.isInteger(a)){
+//     for(var i = 0; i<=a ; i= i + 1){
+
+    
+//    if( i%2 ===0) {
+    
+//    console.log(`${i}-chẵn`);
+// }
+//     }for(var i = 0; i<=a ; i= i + 1){
+//         if( i%2 > 0){
+//             console.log(`${i}-lẻ`);
+//         }
+//     }
+// }
+
+//ex9
+
+
+
+
